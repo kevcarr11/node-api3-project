c9c6233530bc1fe85ec86a28e5d7a53f691e7592
Kevin Carr <kevincarr@Kevins-MacBook-Pro.local>
2019-12-10 17:36:10 -0600

    installed dependencies

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code away!
+const express = require("express")
+const helmet = require("helmet")
+const morgan = require("morgan")
+
+const server = express()
+
+server.use(helmet())
+server.use(morgan("short"))
+server.use(express.json())
+
+
+
+server.use((req, res) => {
+  res.status(404).json({
+    message: "Page Not Found",
+  })
+})
+
+server.listen(8080, () => {
+  console.log("Server Running on http://localhost:8080")
+})
